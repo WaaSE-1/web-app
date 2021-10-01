fcd6f719f0b13bed05217134be5735a17cb84cf9
NielsBleijerveld <bleijerveld.n@gmail.com>
2021-10-01 15:12:35 +0200

    create basic login form and style (CSS) file

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,21 @@
+import '../css/Login.css'
+
+const handleLoginSubmit = (e) => {
+  e.preventDefault();
+  alert("Starting the login process")
+}
+
 const Login = () => {
-  return <h1>Login</h1>
+  return (
+      <form className="login-form">
+        <label for="email">Email:</label><br />
+        <input type="text" id="email" name="email" /><br />
+        <label for="password">Password:</label><br />
+        <input type="text" id="password"/><br />
+        <input type="submit" value="Submit" onClick={handleLoginSubmit}></input>
+      </form>
+    );
+
 }
 
 export default Login;
