badabf95bf09ca445f1bd9e6b58883d16a18c546
Crap-Head <sebastian7307@hotmail.com>
2021-10-01 16:08:42 +0200

    Initial register form front-end

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,41 @@
+import '../css/Register.css'
+
+const handleRegisterSubmit = (e) => {
+  e.preventDefault();
+
+  alert(e.firstname[0].value);
+}
+
 const Register = () => {
-  return <h1>Register</h1>
+  return (
+    <div>
+      <h3>Please fill in your information below to register a user</h3>
+      <form className="register-form">
+        <label for="firstname">First Name:</label><br/>
+        <input type="text" id="firstname" name="firstname"/><br/><br/>
+
+        <label for="lastname">Last Name:</label><br/>
+        <input type="text" id="lastname" name="lastname"/><br/><br/>
+
+        <label for="email">Email:</label><br/>
+        <input type="text" id="email" name="email"/><br/><br/>
+
+        <label for="phonenumber">Phone Number:</label><br/>
+        <input type="text" id="phonenumber" name="phonenumber"/><br/><br/>
+
+        <label for="address">Street Address:</label><br/>
+        <input type="text" id="address" name="address"/><br/><br/>
+
+        <label for="postcode">Post Code:</label><br/>
+        <input type="text" id="postcode" name="postcode"/><br/><br/>
+
+        <label for="password">Password:</label><br/>
+        <input type="text" id="password"/><br/>
+
+        <input type="submit" value="Submit" onClick={handleRegisterSubmit}></input>
+      </form>
+    </div>
+    );
 }
 
 export default Register;
