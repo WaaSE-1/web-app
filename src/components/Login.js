162071aa6d69945fab1798e66cde368daf3bec1a
Toms Audrins <toms.audrins@gmail.com>
2021-10-02 10:05:44 +0200

    Refactor the login request

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,12 +3,8 @@ import Request from '../scripts/request'
 
 const handleLoginSubmit = (e) => {
   e.preventDefault();
-  const formData = new FormData(document.querySelector('form'))
-  console.log(formData.entries())
-  Request("POST", "/users/login", {
-        email: document.getElementById("email").value,
-        password: document.getElementById("password").value
-    }).then(data => {
+  const formData = Object.fromEntries(new FormData(document.querySelector("form")))
+  Request("POST", "/users/login", formData).then(data => {
         let infoBar = document.getElementsByClassName("info-message")[0]
         infoBar.style.visibility = "visible"
         infoBar.style.color = data.success ? "#5ffd5f" : "#ff9999"
@@ -27,7 +23,7 @@ const Login = () => {
         </div>
         <div className="input-group">
           <label htmlFor="password">Password</label><br />
-          <input type="password" id="password" placeholder="********" required/><br />
+          <input type="password" id="password" name="password" placeholder="********" required/><br />
         </div>
         <p className="info-message">Hey</p>
         <input className="button" type="submit" value="Login" onClick={handleLoginSubmit}></input>
