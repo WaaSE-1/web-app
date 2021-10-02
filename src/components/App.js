a91ca071ad46421b89cf22eac6eb9529ba8f54a7
Corinatu <90773342+Corinatu@users.noreply.github.com>
2021-10-02 11:27:07 +0200

    Add route to account and component to settings

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import '../css/App.css';
 import Navbar from './Navbar'
 import Register from "./Register"
 import Login from './Login'
+import Settings from './Settings'
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
@@ -13,6 +15,8 @@ const App = () => {
           <Switch>
             <Route path="/user/register" component={Register} />
             <Route path="/user/login" component={Login} />
+            <Route path="/user/account" component={Settings} />
+
           </Switch>
         </div>
       </Router>
