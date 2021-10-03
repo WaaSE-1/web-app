1d3f9a6844cf7ea6b63cb24807fb8b618132f99a
Toms Audrins <toms.audrins@gmail.com>
2021-10-03 09:09:13 +0200

    Allow users to modify input fields in update account page

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,14 +1,19 @@
 import Request from '../scripts/request' 
 import { UserContext } from '../contexts/UserContext.js'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import jwt from 'jwt-decode'
 
 const Settings = () => {
 
   const {token, setToken} = useContext(UserContext)
-  console.log(jwt(token))
-  const handleDeleteSubmit = (e) => {
-      
+  const [firstname, setFirstName] = useState(jwt(token).firstname)
+  const [lastname, setLastName] = useState(jwt(token).lastname)
+  const [email, setEmail] = useState(jwt(token).email)
+  const [phone_number, setPhoneNumber] = useState(jwt(token).phonenumber)
+  const [address, setAddress] = useState(jwt(token).address)
+  const [location_id, setLocationID] = useState(jwt(token).loc_id)
+
+  const handleDelete = (e) => {
       e.preventDefault();
       Request("DELETE", "/users/delete", {"email": jwt(token).email}, 'Bearer ' + token).then(data => {
             if (data.success) {
@@ -17,41 +22,52 @@ const Settings = () => {
         })
     }
 
+  const handleUpdate = (e) => {
+      e.preventDefault();
+      Request("PUT", '/users/update', Object.fromEntries(new FormData(document.querySelector("form"))), 'Bearer ' + token).then(data => {
+            console.log(data)
+        })
+    }
+
   return ( // When refactoring just make it a component, same as register, and value depends on if JWT token is set
-      <div class='register-form'>
-        <div class='row'>
-          <div class='column'>
+      <div className='register-form'>
+        <div className='row'>
+          <div className='column'>
             <form className="">
             <div className="input-group">
               <label htmlFor="firstname">First Name:</label><br/>
-              <input type="text" id="firstname" value={jwt(token).firstname} name="firstname"/><br/><br/>
+              <input type="text" id="firstname" value={firstname} name="firstname" onChange={e => setFirstName(e.target.value)}/><br/><br/>
             </div>
             <div className="input-group">
               <label htmlFor="lastname">Last Name:</label><br/>
-              <input type="text" id="lastname" value={jwt(token).lastname} name="lastname"/><br/><br/>
+              <input type="text" id="lastname" value={lastname} name="lastname" onChange={e => setLastName(e.target.value)}/><br/><br/>
             </div>
             <div className="input-group">
               <label htmlFor="email">Email:</label><br/>
-              <input type="text" id="email" value={jwt(token).email} name="email"/><br/><br/>
+              <input type="text" id="email" value={email} name="email" onChange={e => setEmail(e.target.value)}/><br/><br/>
             </div>
             <div className="input-group">
               <label htmlFor="phonenumber">Phone Number:</label><br/>
-              <input type="text" id="phonenumber" value={jwt(token).phone_number} name="phonenumber"/><br/><br/>
+              <input type="text" id="phonenumber" value={phone_number} name="phonenumber" onChange={e => setPhoneNumber(e.target.value)}/><br/><br/>
             </div>
             <div className="input-group">
               <label htmlFor="address">Street Address:</label><br/>
-              <input type="text" id="address" name="address" value={jwt(token).address}/><br/><br/>
+              <input type="text" id="address" name="address" value={address} onChange={e => setAddress(e.target.value)}/><br/><br/>
             </div>
             <div className="input-group">
               <label htmlFor="loc_id">Post Code:</label><br/>
-              <input type="text" id="loc_id" name="loc_id" value={jwt(token).location_id}/><br/><br/>
+              <input type="text" id="loc_id" name="loc_id" value={location_id} onChange={e => setLocationID(e.target.value)}/><br/><br/>
             </div>
-            <input className="button" type="submit" value="Update details" onClick={(e) => {e.preventDefault(); alert("Not implemented")}}></input>
+            <div className="input-group">
+              <label htmlFor="loc_id">Password:</label><br/>
+              <input type="text" id="password" name="password"/><br/><br/>
+            </div>
+            <input className="button" type="submit" value="Update details" onClick={handleUpdate}></input>
           </form>
           </div>
-          <div class='column'>
-            <div class='green-column register-form'>
-              <input className="button register" type="submit" value="Delete my account!" onClick={handleDeleteSubmit}></input><br />
+          <div className='column'>
+            <div className='green-column register-form'>
+              <input className="button register" type="submit" value="Delete my account!" onClick={handleDelete}></input><br />
               <input className="button" type="submit" value="Logout" onClick={() => setToken(null)}></input>
             </div>
           </div>
