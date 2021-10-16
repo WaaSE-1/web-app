0a123b28c7dcb22b404883bcc3fcc36d7e03f123
Toms Audrins <toms.audrins@gmail.com>
2021-10-16 18:05:46 +0200

    Create a new car for user in their garage functionality

--- src/components/Garage.js
+++ src/components/Garage.js
@@ -1,5 +1,6 @@
 import {useState, useEffect} from 'react'
 import Request from '../scripts/request'
+import jwt from 'jwt-decode'
 
 const Garage = ({token}) => {
     const [cars, setCars] = useState([])
@@ -10,6 +11,40 @@ const Garage = ({token}) => {
       })
     }, [token])
     
+    const createCarForUser = (e) => {
+        e.preventDefault()
+        console.log(token)
+        console.log(jwt(token))
+        let formData = {"price": 0, ...Object.fromEntries(new FormData(document.querySelector("form")))}
+        let infoBar = document.getElementsByClassName("info-message")[0]
+        console.log(formData)
+        if (Object.values(formData).some(e => e === '')) {
+            console.log('empty', Object.values(formData))
+            infoBar.style.visibility = "visible"
+            infoBar.textContent = "Please fill out all of the fields!"
+            return
+        }
+        Request("POST", "/cars/", formData, 'Bearer ' + token).then(data => {
+          if(data.success){
+              console.log(data)
+
+              formData = {"customer_id": jwt(token).id, "vehicle_id": data.id, ...formData}
+              console.log('new form', formData)
+               Request("POST", "/users/garage", formData, 'Bearer ' + token).then(data => {
+                infoBar.style.visibility = "visible"
+                infoBar.style.color = data.success ? "#5ffd5f" : "#ff9999"
+                infoBar.textContent = data.success ? data.success : data.error
+                if (data.success) {
+                    Request("GET", "/users/garage", {}, 'Bearer ' + token).then(data => {
+                        setCars(data)
+                    })
+                }
+                console.log(data)
+               })
+          }
+      })
+    }
+
     return (
         <div className="grid register-form">
             <div className="cars">
@@ -19,9 +54,28 @@ const Garage = ({token}) => {
             <div className="addCar">
                 Add a new car
                 <form>
-                    <input type="text" placeholder="VIN number"/>
-                    <input type="text" placeholder="Vehicle ID"/>
-                    <input type="text" placeholder="License Plate"/>
+                    <div className="input-group">
+                        <label htmlFor="manufacturer">Manufacturer:</label><br/>
+                        <input type="text" id="manufacturer" name="manufacturer" placeholder="Tesla"/><br/><br/>
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="manufacturer">Model:</label><br/>
+                        <input type="text" id="model" name="model" placeholder="Model 3"/><br/><br/>
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="year">Year:</label><br/>
+                        <input type="number" id="year" name="year" placeholder="year"/><br/><br/>
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="VIN">VIN number:</label><br/>
+                        <input type="text" id="VIN" name="VIN" placeholder="SKAP5000TES"/><br/><br/>
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="manufacturer">License plate:</label><br/>
+                        <input type="text" id="manufacturer" name="license_plate" placeholder="BMW"/><br/><br/>
+                    </div>
+                    <p className="info-message">Hey</p>
+                    <button type="button" className="button" onClick={(e) => createCarForUser(e)}>Add a car</button>
                 </form>
             </div>
         </div>
