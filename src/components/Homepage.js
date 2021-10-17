61611c3981bd919dda870535ba1b506dd4b3763a
Corinatu <90773342+Corinatu@users.noreply.github.com>
2021-10-17 14:27:38 +0200

    Creted the homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,12 +1,24 @@
+import '../css/Home.css'
+import '../css/App.css'
+import '../css/Register.css'
+import '../css/CarsSingle.css'
+
 export const Homepage = () => {
+
     return (
-        <div>
-            <h1>Hellstern Auto</h1>
-            <p>Software engineering elective exam project</p>
-            <p>Backend - Python + FastAPI</p>
-            <p>Frontend - React</p>
-            <p>Database - MySQL</p>
-            <p>Host - Azure</p>
+        <div className="register-form">
+            <img width="1000" height="400" alt="
+            " src="https://media.istockphoto.com/vectors/black-friday-luxury-sale-banner-black-background-with-papercut-layers-vector-id1048020866?k=20&m=1048020866&s=170667a&w=0&h=RwYyX1WmmGk3cbfnR-7Re8KC9bVamYaOXg6qsH_yfCE="/>
+            <h2> See our products under car and services section! The sales will take place on the Black Friday weekend only. </h2>
+            <div className="grid-car2">
+                <img width="400" height="180" alt="" src="https://www.porsche.dk/globalassets/modeller/cayenne/de-nye-cayenne-coupe/galleri/galleri29_stor.jpg" />
+                    <h4> Porche Cayenne-e-hybrid-coupe. Our new arrival. Make sure to check it out! A lot of horse power out there! </h4>
+                
+            </div>
+
+
+                
+                
         </div>
     )
 }
