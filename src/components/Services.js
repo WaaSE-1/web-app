bc856756993dd5f6c45de977ad0cb1f8e734dab9
Toms Audrins <toms.audrins@gmail.com>
2021-10-18 22:14:56 +0200

    Create a service request functionality

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,6 +3,29 @@ import Request from '../scripts/request'
 import { CarDetails } from './CarDetails'
 import Loader from "react-loader-spinner";
 
+
+const createServiceRequest = (e) => {
+    e.preventDefault()
+    
+    let formData = Object.fromEntries(new FormData(document.querySelector("form")))
+    let infoBar = document.getElementsByClassName("info-message")[0]
+    const currentDate = new Date()
+    formData = {"date": currentDate.toISOString().split('T')[0], ...formData}
+    console.log(formData)
+    if (Object.values(formData).some(e => e === '')) {
+            console.log('empty', Object.values(formData))
+            infoBar.style.visibility = "visible"
+            infoBar.textContent = "Please fill out all of the fields!"
+            return
+        }
+
+    Request("POST", "/services/requests", formData).then(data => {
+          infoBar.style.visibility = "visible"
+          infoBar.style.color = data.success ? "#5ffd5f" : "#ff9999"
+          infoBar.textContent = data.success ? data.success : data.error
+      })
+} 
+
 export const Services = ({services, token}) => {
     const [cars, setCars] = useState([])
     useEffect(() => {
@@ -19,8 +42,7 @@ export const Services = ({services, token}) => {
             <div className="customer-section">
                 <div className="customer-cars">
                     <h1 className="header-title">Your cars</h1>
-                    {console.log(cars)}
-                    {cars.length === 0 ? <Loader color="#1e1e2f" type="ThreeDots"/> : cars.map(car => <CarDetails key={car.vehicle_ident_number} car={car} />) }
+                    {cars.length === 0 ? cars.length === 0 && services.length > 0 ? "No cars available!" : <Loader color="#1e1e2f" type="ThreeDots"/> : cars.map(car => <CarDetails key={car.vehicle_ident_number} car={car} />) }
                 </div>
             </div>
             <div className="book-a-service">
@@ -29,18 +51,19 @@ export const Services = ({services, token}) => {
                     {cars.length !== 0 && services.length !== 0 ? (<form >
                         <div className="input-group">
                             <label htmlFor="services">Service type</label>
-                            <select name="services" id="services">
-                                {services.map(service => <option key={service.dealership + service.service_type} value={service.service_type + " at " + service.dealership}>{service.service_type + " at " + service.dealership + " (DKK " + service.price + ")"}</option>)}
+                            <select name="service" id="service">
+                                {services.map(service => <option key={service.dealership + service.service_type} value={service.id}>{service.service_type + " at " + service.dealership + " (DKK " + service.price + ")"}</option>)}
                             </select>
                         </div>
                         <div className="input-group">
                             <label htmlFor="cars">Vehicle</label>
-                            <select name="cars" id="cars">
-                                {cars.map(car => <option key={car.vehicle_ident_number} value={car.company_name + " " + car.model + ", " + car.license_plate}>{car.company_name + " " + car.model + ", " + car.license_plate}</option>)}
+                            <select name="VIN" id="VIN">
+                                {cars.map(car => <option key={car.vehicle_ident_number} value={car.vehicle_ident_number}>{car.company_name + " " + car.model + ", " + car.license_plate}</option>)}
                             </select>
                         </div>
+                        <p className="info-message">Hey</p>
                         <div className="input-group">
-                            <button type="button" className="button buy" onClick={() => alert("Not implemented")}>Create service request</button>
+                            <button type="button" className="button buy" onClick={(e) => createServiceRequest(e)}>Create service request</button>
                         </div>
                     </form>) : cars.length === 0 && services.length > 0 ? "No cars available!" : <Loader color="#1e1e2f" type="ThreeDots"/>}
                 </div>
